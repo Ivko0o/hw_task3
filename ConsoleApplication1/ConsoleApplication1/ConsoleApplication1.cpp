883b3029e0dbf9608504819e8ed5ae970ef8a9f9
Ivko0o <IvelinVasilev91@gmail.com>
2024-10-15 14:49:28 +0300

    Final version

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,28 +6,28 @@ using namespace std;
 int monthDays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 bool isLeapYear(int year);
-int dateToNumber(int day, int month, int year);
+unsigned dateToNumber(unsigned day, unsigned month, unsigned year);
 
 int main()
 {
-	unsigned int inputNumber = 0;
-	int day = 0;
-	int month = 0;
-	int year = 0;
-	int lengthExpiryDate = 0;
-	int currentDay = 1;
-	int currentMonth = 11;
-	int currentYear = 2023;
-	int countControlBits = 0;
+	unsigned inputNumber = 0;
+	unsigned day = 0;
+	unsigned month = 0;
+	unsigned year = 0;
+	unsigned lengthExpiryDate = 0;
+	unsigned currentDay = 1;
+	unsigned currentMonth = 11;
+	unsigned currentYear = 2023;
+	unsigned countControlBits = 0; // counts number of 1 bits
 
-	int check_Day = 31;
-	int check_Month = 480;
-	int check_Year = 261632;
-	int check_Length_Expiry_Date = 2147418112;
-	int check_Control_Bits = 1;
+	unsigned check_Day = 31;
+	unsigned check_Month = 480;
+	unsigned check_Year = 261632;
+	unsigned check_Length_Expiry_Date = 2147418112;
+	unsigned check_Control_Bits = 1;
 
 	//Takes a number from the user
-	cout << "Enter a whole positive number to check the expiry date of a product: ";
+	std::cout << "Enter a whole positive number to check the expiry date of a product: ";
 	cin >> inputNumber;
 
 	//Checks the day of the expiry date
@@ -40,20 +40,44 @@ int main()
 	//Checks the year of the expiry date
 	year = (inputNumber & check_Year) >> 9;
 
-	//Checks the if the day and month are appropriate format
-	if (day > 31) {
-		cout << "\nInvalid!";
-		return 0;
+	//Checks the number of controlled bits
+	for (int i = 0; i < 31; i++) {
+		if ((inputNumber & check_Control_Bits) != 0) {
+			countControlBits++;
+		}
+		check_Control_Bits <<= 1;
 	}
-	else if (month > 12) {
+
+	//Checks the most significant bit and turn is to 1 if the number of control bits is odd
+	if (countControlBits % 2 == 1) {
+		inputNumber = inputNumber | (1 << 31);
+	}
+
+
+	//Checks the if the day and month are appropriate format
+	if (day > 31 || day < 1) {
 		cout << "\nInvalid!";
-		return 0;
+		return -1;
+	}
+	else if (month > 12 || month < 1) {
+		cout << "\nInvalid!";
+		return -1;
+	}
+	else if (year > 123 || year < 0) {
+		cout << "\nInvalid!";
+		return -1;
+	}
+
+	//Checks if the production date is after current date
+	if (day > currentDay && month > currentMonth && year > currentYear) {
+		cout << "Not possible!";
+		return -1;
 	}
 
 	//Turns the days to numbers
-	int days1 = dateToNumber(currentDay,currentMonth,currentYear);
-	int days2 = dateToNumber(day, month, year+1900);
-	int difference = days1 - days2;
+	unsigned days1 = dateToNumber(currentDay, currentMonth, currentYear);
+	unsigned days2 = dateToNumber(day, month, year + 1900);
+	unsigned difference = days1 - days2;
 
 
 	cout << "\nCurrent date is: 0" << currentDay << "." << currentMonth << "." << currentYear;
@@ -64,17 +88,7 @@ int main()
 	lengthExpiryDate = (inputNumber & check_Length_Expiry_Date) >> 18;
 	cout << "\n" << "Length of the expiry date in days: " << lengthExpiryDate;
 
-
-
-	//Checks the number of controlled bits
-	for (int i = 0; i < 31; i++) {
-		if ((inputNumber & check_Control_Bits) != 0) {
-			countControlBits++;
-		}
-		check_Control_Bits += check_Control_Bits;
-	}
-
-	cout << "\n\nThe number of control bits is: " << countControlBits;
+	std::cout << "\n\nThe number of control bits is: " << countControlBits;
 
 
 	//Checks the most significant bit
@@ -85,16 +99,6 @@ int main()
 		if ((inputNumber & (1 << 31)) != 0) {
 			cout << "\n\nInvalid!";
 			return 0;
-		}
-	}
-
-	//Checks the most significant bit and turn is to 1 if the number of control bits is odd
-	if (countControlBits % 2 == 1) {
-		if ((inputNumber & (1 << 31)) != 1) {
-			cout << "\nThe number is correct!";
-		}
-		if ((inputNumber & (1 << 31)) != 1) {
-			inputNumber = inputNumber ^ (1 << 31);
 		}
 	}
 
@@ -117,8 +121,8 @@ bool isLeapYear(int year) {
 }
 
 //Turns the date into a number
-int dateToNumber(int day, int month, int year) {
-	int days = year * 365 + day;
+unsigned dateToNumber(unsigned day, unsigned month, unsigned year) {
+	unsigned days = year * 365 + day;
 
 	for (int i = 0; i < month - 1; i++) {
 		days += monthDays[i];
@@ -132,4 +136,3 @@ int dateToNumber(int day, int month, int year) {
 
 	return days;
 }
-
